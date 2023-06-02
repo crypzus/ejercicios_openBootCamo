311030db6f0a05cc59d235adb58cd9d099a7091e
crypzus <mejiasernestor@gmail.com>
2023-06-02 16:08:13 -0500

    se agrego la descripciones del ejercicio

--- lista-de-elementos.js
+++ lista-de-elementos.js
@@ -1,3 +1,15 @@
+/* Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url) */
+
 
 const myDate = {
   name: "Ernestor Mejias",
