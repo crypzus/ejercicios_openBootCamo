0c3f1eaf6c67b248604e47d957d498149944633a
crypzus <mejiasernestor@gmail.com>
2023-06-02 15:47:45 -0500

    ejercicio lista de elementos

---
+++ lista-de-elementos.js
@@ -0,0 +1,14 @@
+
+const myDate = {
+  name: "Ernestor Mejias",
+  edad: 32,
+  desarrollador: true,
+  fechaNacimiento: new Date(1990, 6, 18),
+  myFavoriteBook: {
+    titulo: "el hombre mas rico de babilonia",
+    autor: "George S. Clason",
+    fecha: 1926,
+    url: "https://irp-cdn.multiscreensite.com",
+  },
+};
+console.log(myDate);
