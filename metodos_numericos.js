474c59de1c6d2b6dcf5f5b2bc97a07cecedafc17
crypzus <mejiasernestor@gmail.com>
2023-06-07 13:50:45 -0500

    ejercicio de metodos numericos

---
+++ metodos_numericos.js
@@ -0,0 +1,34 @@
+//metodos numericos y precision 
+// Crea un archivo JS que contenga las siguientes líneas
+
+// - Una variable que contenga tu altura en centímetros (entero)
+ let alturaCm =  169;
+
+//  - Una variable que contenga tu altura en metros (número de coma flotante)
+ let alturaMtr = 1.69;
+ 
+// - Una variable que contenga tu peso en kilogramos (número de coma flotante)
+ let pesoKg = 94.5;
+
+//  - Una variable que contenga tu altura en metros redondeada hacia arriba
+ let alturaRedondeadaUp = alturaMtr.toFixed(1);
+
+//  - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+ let pesoKgRedondeadoDown = Math.floor(pesoKg);
+
+ 
+// - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+ let maxValor = Number.MAX_VALUE;
+ let maxValorMasUno = maxValor + 1;
+ let igualdad = (maxValor === maxValorMasUno)
+ 
+
+
+
+
+
+
+
+
+
+
