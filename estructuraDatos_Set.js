033bb65e93091d3951e4e64c2aecd76bb480be8a
crypzus <mejiasernestor@gmail.com>
2023-06-20 18:14:54 -0500

    ejercicio de estructura de datos Set

---
+++ estructuraDatos_Set.js
@@ -0,0 +1,17 @@
+
+
+
+// - Un nuevo Set con los nombres de tu familia
+
+
+const family = new Set(["Malvis", "Marcos", "Cecilia", "Maria", "Raziel", "Cristobal"]);
+console.log(family);
+
+
+// - Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+family.add("Marcos")
+console.log(family);
+
+// - Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+family.add("Javascript")
+console.log(family);
